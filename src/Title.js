704e269c6fa0537106fa2fe764b0651e603754cf
darkshadow <shoaib.mehedi@gmail.com>
2019-01-31 17:17:12 +0600

    first commit

---
+++ src/Title.js
@@ -0,0 +1,158 @@
+import React, { Component } from "react";
+import "./Title.css";
+
+class Title extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "Test",
+      name: "MD Shoaib Mehedi",
+      mail: "shoiab.mehedi@gmail.com",
+      phone: "01725991536",
+      designation: "BAAL",
+      department: "RokkhiWeb",
+      isInput: false
+    };
+  }
+
+  editHandler() {
+    this.setState({
+      ...this.state,
+      isInput: true
+    });
+  }
+
+  titleInputChange(event) {
+    this.setState({
+      ...this.state,
+      title: event.target.value
+    });
+  }
+  nameInputChange(event) {
+    this.setState({
+      ...this.state,
+      name: event.target.value
+    });
+  }
+  mailInputChange(event) {
+    this.setState({
+      ...this.state,
+      mail: event.target.value
+    });
+  }
+  phoneInputChange(event) {
+    this.setState({
+      ...this.state,
+      phone: event.target.value
+    });
+  }
+  designationInputChange(event) {
+    this.setState({
+      ...this.state,
+      designation: event.target.value
+    });
+  }
+  departmentInputChange(event) {
+    this.setState({
+      ...this.state,
+      department: event.target.value
+    });
+  }
+
+  keyPressHandler(event) {
+    if (event.key === "Enter") {
+      this.setState({
+        ...this.state,
+        isInput: false
+      });
+    }
+  }
+
+//   blurHandler(event) {
+//     this.setState({
+//       ...this.state,
+//       isInput: false
+//     });
+//   }
+
+  render() {
+    let output = null;
+
+    if (this.state.isInput) {
+      output = (
+        <div className="Title">
+          <input
+            className="form-control"
+            onChange={event => this.titleInputChange(event)}
+            onKeyPress={event => this.keyPressHandler(event)}
+            // onBlur={event => this.blurHandler(event)}
+            type="text"
+            value={this.state.title}
+          />
+          <input
+            className="form-control"
+            onChange={event => this.nameInputChange(event)}
+            onKeyPress={event => this.keyPressHandler(event)}
+            // onBlur={event => this.blurHandler(event)}
+            type="text"
+            value={this.state.name}
+          />
+          <input
+            className="form-control"
+            onChange={event => this.mailInputChange(event)}
+            onKeyPress={event => this.keyPressHandler(event)}
+            // onBlur={event => this.blurHandler(event)}
+            type="text"
+            value={this.state.mail}
+          />
+          <input
+            className="form-control"
+            onChange={event => this.phoneInputChange(event)}
+            onKeyPress={event => this.keyPressHandler(event)}
+            // onBlur={event => this.blurHandler(event)}
+            type="text"
+            value={this.state.phone}
+          />
+          <input
+            className="form-control"
+            onChange={event => this.designationInputChange(event)}
+            onKeyPress={event => this.keyPressHandler(event)}
+            // onBlur={event => this.blurHandler(event)}
+            type="text"
+            value={this.state.designation}
+          />
+          <input
+            className="form-control"
+            onChange={event => this.departmentInputChange(event)}
+            onKeyPress={event => this.keyPressHandler(event)}
+            // onBlur={event => this.blurHandler(event)}
+            type="text"
+            value={this.state.department}
+          />
+        </div>
+      );
+    } else {
+      output = (
+        <div className="d-flex Title">
+          <p className="display-4"> {this.state.title} </p>
+          <p className="display-4"> {this.state.name} </p>
+          <p className="display-4"> {this.state.mail} </p>
+          <p className="display-4"> {this.state.phone} </p>
+          <p className="display-4"> {this.state.designation} </p>
+          <p className="display-4"> {this.state.department} </p>
+          <span
+            onClick={() => this.editHandler()}
+            className="ml-auto edit-icon"
+          >
+            <i className="fas fa-pencil-alt" />
+          </span>
+        </div>
+      );
+    }
+
+    return <div>{output}</div>;
+  }
+}
+
+export default Title;
